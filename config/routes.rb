c996de93ba70e05d9b80cd2fb98f6a1f14ec2117
kz-oita <taoi.kizuka@gmail.com>
2020-01-19 22:35:34 +0900

    create-user-info

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   
   root "messages#index"
+=======
+
+  devise_for :users
+  root to: "messages#index"
+  resources :users, only: [:edit, :update]
+>>>>>>> Stashed changes
 end
