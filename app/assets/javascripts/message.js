b75435d208ac7b24c61f82f72815070cf489bd34
kz-oita <taoi.kizuka@gmail.com>
2020-02-03 19:16:36 +0900

    自動更新機能の修正２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -58,30 +58,4 @@ $(function(){
       alert('error');
     })
   })
-  var reloadMessages = function() {
-    last_message_id = $('.main-chat__message-lists__content:last').data("message-id");
-    console.log(last_message_id);
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      if (messages.length !==0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        $('.main-chat__message-list').append(insertHTML);
-        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error')
-    });
-  };
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-  }
 });
