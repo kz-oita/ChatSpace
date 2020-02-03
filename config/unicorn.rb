464553382c7556f81d11125d3521d979bcbd4988
kz-oita <taoi.kizuka@gmail.com>
2020-02-04 07:42:49 +0900

    本番環境実装の修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 
-working_directoy app_path
+working_directory app_path
 
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
@@ -15,31 +15,31 @@ stdout_path "#{app_path}/log/unicorn.stdout.log"
 timeout 60
 
 preload_app true
-GC.respond to_?(:copy_on_write_friendly=) && GC.copy_on_write_friendly =true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
 check_client_connection false
 
 run_once = true
 
-before_fork do |server,worker|
+before_fork do |server, worker|
   defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
 
   if run_once
-    run_once = false
+    run_once = false # prevent from firing again
   end
 
-  old_pid ="#{server.config[:pid]}.oldbin"
+  old_pid = "#{server.config[:pid]}.oldbin"
   if File.exist?(old_pid) && server.pid != old_pid
     begin
       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENONET, Errno::ESRCH => e
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
       logger.error e
     end
   end
 end
-      
-after_fork do |_server,_worker|
+
+after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
